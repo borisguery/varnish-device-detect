e6c454b959e801d59c812b1c74f8c3d73ce51e43
Boris Guéry <guery.b@gmail.com>
2013-11-27 13:55:08 +0100

    Generate tests in batch because of f***g weird error I got when running tests (signal=11) which seems caused when the tests number exceed 75
    * Update Makefile accordingly to the new command syntax

--- Makefile
+++ Makefile
@@ -2,9 +2,9 @@
 
 test: clean
 	@echo "[test]\t- Generating tests from control set"
-	@$(PWD)/tests/devicedetect/resources/controlset-to-vtc.py $(PWD)/tests/devicedetect/resources/controlset.csv > $(PWD)/tests/devicedetect/100-from-controlset.vtc
+	@$(PWD)/tests/devicedetect/resources/controlset-to-vtc.py -s 70 $(PWD)/tests/devicedetect/resources/controlset.csv $(PWD)/tests/devicedetect/
 	@/usr/bin/varnishtest -Dvarnishd=/usr/sbin/varnishd -Dprojectdir=$(PWD) tests/**/*.vtc
 
 clean:
 	@echo "[clean]\t- Cleaning generated tests"
-	@rm $(PWD)/tests/devicedetect/100-from-controlset.vtc
+	@rm -f $(PWD)/tests/devicedetect/9*from-controlset.vtc
--- tests/devicedetect/resources/controlset-to-vtc.py
+++ tests/devicedetect/resources/controlset-to-vtc.py
@@ -3,7 +3,7 @@
 # Generate a varnishtest script to validate the preclassified
 # U-A strings in the control set.
 
-import errno, sys
+import errno, sys, argparse
 
 HEADER="""varnishtest "automatic test of control set"
 server s1 {
@@ -30,11 +30,20 @@ TAILER="""
 client c1 -run
 """
 
-def main():
+def flush(batchNum, header, buff, tailer, outputdir="."):
+    path = outputdir + "9" + str(batchNum).zfill(4) + "-from-controlset.vtc"
+    with open(path, "w+") as f:
+        f.write(header)
+        f.write(buff)
+        f.write(tailer)
+
+def main(inputfile, outputdir, splitcount=76):
     try:
-        with open(sys.argv[1]) as controlset:
+        with open(inputfile) as controlset:
             lines = controlset.readlines()
-            print HEADER
+            buff = ""
+            i = 1
+            batchNum = 1
             for line in lines:
                 if line.startswith("#"):
                     continue
@@ -44,15 +53,31 @@ def main():
                 if len(line) == 0:
                     continue
 
+                flushed = False
                 typeId, OScode, UAString = line.split("|", 2)
-                print "\ttxreq -hdr \"User-Agent: %s\"" % UAString
-                print "\trxresp"
-                print "\texpect resp.http.X-UA-Device-Type == \"%s\"" % typeId
-                print "\texpect resp.http.X-UA-Device-OS   == \"%s\"" % OScode
-                print "\n" # for readability
-            print TAILER
+                buff = buff + "\ttxreq -hdr \"User-Agent: %s\"\n" % UAString
+                buff = buff + "\trxresp\n"
+                buff = buff + "\texpect resp.http.X-UA-Device-Type == \"%s\"\n" % typeId
+                buff = buff + "\texpect resp.http.X-UA-Device-OS   == \"%s\"\n" % OScode
+                buff = buff + "\n" # for readability
+
+                if (0 == ((i + 1) % splitcount)):
+                    flush(batchNum, HEADER, buff, TAILER, outputdir)
+                    flushed = True
+                    batchNum = batchNum + 1
+                    buff = ""
+                i = i + 1
+
+        if (False == flushed):
+            flush(batchNum, HEADER, buff, TAILER, outputdir)
+
     except IOError:
         print "Error while reading: %s" % sys.argv[1]
 
 if __name__ == "__main__":
-        main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("inputfile", help="Input CSV file", type=str)
+    parser.add_argument("outputdir", help="Output directory", type=str)
+    parser.add_argument("-s", "--splitcount", help="Determine after how much tests we must split the files", type=int, default=76)
+    args = parser.parse_args()
+    main(args.inputfile, args.outputdir, args.splitcount)
